04dbf9c277adc54d8f1902c6e43ecc2bc7a51f10
Muzammil Ali <153710574+muzammilali11@users.noreply.github.com>
2024-03-04 23:29:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const sr = ScrollReveal({
+  distance: "40px",
+  duration: 2500,
+  reset: true,
+});
+
+sr.reveal(".logo", { Delay: 200, origin: "left" });
+sr.reveal(".navbar", { Delay: 400, origin: "top" });
+sr.reveal(".menu-btn", { Delay: 520, origin: "right" });
+sr.reveal("#span", { Delay: 650, origin: "left" });
+sr.reveal(".home-text h1", { Delay: 680, origin: "left" });
+sr.reveal(".home-text p", { Delay: 750, origin: "right" });
+sr.reveal(".main-btn", { Delay: 860, origin: "left" });
+sr.reveal(".share p ", { Delay: 950, origin: "bottom" });
+sr.reveal(".social ", { Delay: 950, origin: "bottom" });
+sr.reveal(".home-img", { Delay: 1000, origin: "right" });
